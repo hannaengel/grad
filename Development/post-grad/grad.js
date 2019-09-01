7b01381ca123288e9cc94fecbb1326b192dba47d
Hanna Engel <hengel@alumni.berklee.edu>
2019-09-01 12:45:48 -0700

    m

---
+++ Development/post-grad/grad.js
@@ -0,0 +1,11 @@
+class A{
+    import React from 'react'
+    
+    const INSERT FUNCTION NAME = props => {
+        return (
+            <div>{props.INSERT PROP VAL HERE}</div>
+        )
+    }
+    
+    export default INSERT FUNCTION NAME
+}
